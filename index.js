a08b1716ad0b9d2991586ad7adb9936077ee40d2
Ian Van Winkle <ianxvanwinkle@gmail.com>
2024-10-08 21:54:55 -0400

    commit 2

--- index.js
+++ index.js
@@ -1,130 +1,106 @@
 // The provided course information.
 const CourseInfo = {
-    id: 451,
-    name: "Introduction to JavaScript"
-  };
-  
-  // The provided assignment group.
-  const AssignmentGroup = {
-    id: 12345,
-    name: "Fundamentals of JavaScript",
-    // the ID of the course the assignment group belongs to
-    course_id: 451,
-    // the percentage weight of the entire assignment group
-    group_weight: 25,
-    assignments: [
-      {
-        id: 1,
-        name: "Declare a Variable",
-        // the due date for the assignment
-        due_at: "2023-01-25",
-        // the maximum points possible for the assignment
-        points_possible: 50
-      },
-      {
-        id: 2,
-        name: "Write a Function",
-        due_at: "2023-02-27",
-        points_possible: 150
-      },
-      {
-        id: 3,
-        name: "Code the World",
-        due_at: "3156-11-15",
-        points_possible: 500
-      }
-    ]
-  };
-  
-  // The provided learner submission data.
-  const LearnerSubmissions = [
-    {
-      learner_id: 125,
-      assignment_id: 1,
-      submission: {
-        submitted_at: "2023-01-25",
-        score: 47
-      }
-    },
-    {
-      learner_id: 125,
-      assignment_id: 2,
-      submission: {
-        submitted_at: "2023-02-12",
-        score: 150
-      }
-    },
-    {
-      learner_id: 125,
-      assignment_id: 3,
-      submission: {
-        submitted_at: "2023-01-25",
-        score: 400
-      }
-    },
-    {
-      learner_id: 132,
-      assignment_id: 1,
-      submission: {
-        submitted_at: "2023-01-24",
-        score: 39
-      }
-    },
-    {
-      learner_id: 132,
-      assignment_id: 2,
-      submission: {
-        submitted_at: "2023-03-07",
-        score: 140
-      }
-    }
-  ];
-  
-  function getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions) {
-    // here, we would process this data to achieve the desired result.
-    try {
-        if (AssignmentGroup)
-    }
-    const result = [
-      {
-        id: 125,
-        avg: 0.985, // (47 + 150) / (50 + 150)
-        1: 0.94, // 47 / 50
-        2: 1.0 // 150 / 150
-      },
-      {
-        id: 132,
-        avg: 0.82, // (39 + 125) / (50 + 150)
-        1: 0.78, // 39 / 50
-        2: 0.833 // late: (140 - 15) / 150
-      }
+    id: 451, name: "Introduction to JavaScript"
+};
+
+// The provided assignment group.
+const AssignmentGroup = {
+    id: 12345, name: "Fundamentals of JavaScript", course_id: 451, group_weight: 25, assignments:
+        [
+            { id: 1, name: "Declare a Variable", due_at: "2023-01-25", points_possible: 50 },
+            { id: 2, name: "Write a Function", due_at: "2023-02-27", points_possible: 150 },
+            { id: 3, name: "Code the World", due_at: "3156-11-15", points_possible: 500 }
+        ]
+};
+
+// The provided learner submission data.
+const LearnerSubmissions =
+    [
+        { learner_id: 125, assignment_id: 1, submission: { submitted_at: "2023-01-25", score: 47 } },
+        { learner_id: 125, assignment_id: 2, submission: { submitted_at: "2023-02-12", score: 150 } },
+        { learner_id: 125, assignment_id: 3, submission: { submitted_at: "2023-01-25", score: 400 } },
+
+        { learner_id: 132, assignment_id: 1, submission: { submitted_at: "2023-01-24", score: 39 } },
+        { learner_id: 132, assignment_id: 2, submission: { submitted_at: "2023-03-07", score: 140 } }
     ];
-  
+
+// Some object to hold results by Learner_id
+// Loop through  learner submissons
+// If this is the first time we see the learner we add them to our object
+// Check if assingnment has a course
+// Check if assingment_id is actually due
+// Get the score the learner_id got
+// if late score - * possible points * .9
+// Get the score overall
+// Get average of individual score
+
+function getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmission) {
+
+    const result = [];
+    let learnerData = {};
+
+    for (let i = 0; i < LearnerSubmission.length; i++) {
+        const { learner_id, ...submission } = LearnerSubmission[i];
+
+        if (!learnerData[learner_id]) {
+            learnerData[learner_id] = [];
+        }
+
+        learnerData[learner_id].push(submission);
+
+    }
+    console.log(learnerData)
     return result;
-  }
-  
-  const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
-  
-  console.log(result);
 
-//   ---------------------------------------------------------------------------------------------------------------
 
-// the ID of the learner for which this data has been collected
+}
 
-// "id": number,
+function getPossiblePoints(AssignmentDetails) {
+    let result = 0;
+    const assign = AssignmentDetails.assignments
 
-// the learner’s total, weighted average, in which assignments
-// with more points_possible should be counted for more
-// e.g. a learner with 50/100 on one assignment and 190/200 on another
-// would have a weighted average score of 240/300 = 80%.
+    for (let i = 0; i < assign.length; i++) {
+        console.log(assign[i].points_possible)
+        result += assign[i].points_possible
+        console.log(result)
+    }
+    return result;
+}
+getPossiblePoints(AssignmentGroup)
 
-// "avg": number,
+function getUserScore(LearnerSubmissions) {
+    let result = 0;
 
-// each assignment should have a key with its ID,
-// and the value associated with it should be the percentage that
-// the learner scored on the assignment (submission.score / points_possible)
+    for (let i = 0; i < LearnerSubmissions.length; i++) {
+        const submissions = LearnerSubmissions[i]
+        const scores = submissions.score
 
-// "assignment_id": number,
+            console.log(submissions.submission.score)
+            result += submissions.submission.score
+            console.log(result)
 
-// if an assignment is not yet due, it should not be included in either
-// the average or the keyed dictionary of scores
+    }
+    return result;
+}
+getUserScore(LearnerSubmissions)
+
+
+// const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
+
+// console.log(result);
+
+// Should come out looking something like this.
+//     const result = [
+//       {
+//         id: 125,
+//         avg: 0.985, // (47 + 150) / (50 + 150)
+//         1: 0.94, // 47 / 50
+//         2: 1.0 // 150 / 150
+//       },
+//       {
+//         id: 132,
+//         avg: 0.82, // (39 + 125) / (50 + 150)
+//         1: 0.78, // 39 / 50
+//         2: 0.833 // late: (140 - 15) / 150
+//       }
+//     ];
